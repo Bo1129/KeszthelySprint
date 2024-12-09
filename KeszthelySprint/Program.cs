690e25c6526d8f2595d23b5df2595c9f33c75ab7
Bo1129 <bohmb.22d@acsjszki.hu>
2024-12-09 13:56:09 +0100

    Add project files.

---
+++ KeszthelySprint/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace KeszthelySprint
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Versenyzo> versenyzok = new();
+
+            foreach (var sor in File.ReadAllLines("eredmenyek.txt"))
+            {
+                versenyzok.Add(new Versenyzo(sor));
+            }
+
+            //2.feladat
+            Console.WriteLine($"2.feladat: A versenyt {versenyzok.Count} versenyző fejezte be");
+
+            //3.feladat
+
+            int junior = 0;
+            foreach (var v in versenyzok)
+            {
+                if (v.kategoria == "elit junior")
+                {
+                    junior++;
+                }
+            }
+            Console.WriteLine($"3.feladat: Versenyzők száma az \"elit junior\" kategóriában: {junior} fő");
+
+            //4.feladat
+            double ossz_eletkor = 0;
+            foreach (var v in versenyzok)
+            {
+                ossz_eletkor += DateTime.Now.Year - v.szuletesi_ev;
+            }
+            Console.WriteLine($"4.feladat: Átlag életkor: {ossz_eletkor / versenyzok.Count} év");
+
+            
+
+        }
+        
+    }
+}
---
+++ KeszthelySprint/Versenyzo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeszthelySprint
+{
+    internal class Versenyzo
+    {
+        public string nev { get; set; }
+        public int szuletesi_ev { get; set; }
+        public int rajtszam { get; set; }
+        public string nem { get; set; }
+        public string kategoria { get; set; }
+        public string[] idok { get; set; }
+
+        public Versenyzo(string sor)
+        {
+            string[] s = sor.Split(';');
+
+            nev = s[0];
+            szuletesi_ev = int.Parse(s[1]);
+            rajtszam = int.Parse(s[2]);
+            nem = s[3];
+            kategoria = s[4];
+            idok = new string[5];
+            idok[0] = s[5];
+            idok[1] = s[6];
+            idok[2] = s[7];
+            idok[3] = s[8];
+            idok[4] = s[9];
+
+        }
+    }
+}
+
